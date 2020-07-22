0a2357112b1b2a9f4721e188d61d8215b31abff8
MrBarak45 <elbaraka.amine2@gmail.com>
2020-07-22 18:38:33 +0200

    added stuff for item

--- src/main/java/com/projetjee/infrastructure/controller/Item/ItemController.java
+++ src/main/java/com/projetjee/infrastructure/controller/Item/ItemController.java
@@ -1,4 +1,44 @@
 package com.projetjee.infrastructure.controller.Item;
 
+import com.projetjee.domain.model.Item;
+import com.projetjee.domain.model.User;
+import com.projetjee.useCases.Item.CreateNewItem;
+import com.projetjee.useCases.Item.ExpandExipryDateForItem;
+import com.projetjee.useCases.Item.GetBiddingHistoryOfItem;
+import com.projetjee.useCases.Item.GetHighestBidderForItem;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ItemController {
+
+    private final CreateNewItem createNewItem;
+    private final ExpandExipryDateForItem expandExipryDateForItem;
+    private final GetBiddingHistoryOfItem getBiddingHistoryOfItem;
+    private final GetHighestBidderForItem getHighestBidderForItem;
+
+    public ItemController(CreateNewItem createNewItem, ExpandExipryDateForItem expandExipryDateForItem, GetBiddingHistoryOfItem getBiddingHistoryOfItem, GetHighestBidderForItem getHighestBidderForItem) {
+        this.createNewItem = createNewItem;
+        this.expandExipryDateForItem = expandExipryDateForItem;
+        this.getBiddingHistoryOfItem = getBiddingHistoryOfItem;
+        this.getHighestBidderForItem = getHighestBidderForItem;
+    }
+
+    @PostMapping("/item/add")
+    public ResponseEntity<String> addItem(@RequestBody ItemDto itemDto) {
+        createNewItem.CreateNewItem(new Item(itemDto.getId(), itemDto.getName(), itemDto.getItemPicUrl(), itemDto.getCreationDate(), itemDto.getSeller(), itemDto.getPlacedBids(), itemDto.getHighestCurrentBid()));
+        return new ResponseEntity<>("Item created", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/item/getHighestBidder/{id}")
+    public ResponseEntity<String> getHighestBidderForItem(@RequestBody Item itemDto) {
+        User a = getHighestBidderForItem.GetHighestBidderForItem(itemDto.getId());
+        return new ResponseEntity<>("Highest bidder" + a.toString(), HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/projetjee/infrastructure/controller/Item/ItemDto.java
+++ src/main/java/com/projetjee/infrastructure/controller/Item/ItemDto.java
@@ -1,4 +1,56 @@
 package com.projetjee.infrastructure.controller.Item;
 
+import com.projetjee.domain.model.Bid;
+import com.projetjee.domain.model.User;
+
+import java.net.URL;
+import java.sql.Date;
+
 public class ItemDto {
+    private final Long id;
+    private final String name;
+    private final URL itemPicUrl;
+    private final Date creationDate;
+
+    private final User seller;
+    private final Bid[] placedBids;
+    private final Bid highestCurrentBid;
+
+    public ItemDto(Long id, String name, URL itemPicUrl, Date creationDate, User seller, Bid[] placedBids, Bid highestCurrentBid) {
+        this.id = id;
+        this.name = name;
+        this.itemPicUrl = itemPicUrl;
+        this.creationDate = creationDate;
+        this.seller = seller;
+        this.placedBids = placedBids;
+        this.highestCurrentBid = highestCurrentBid;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public URL getItemPicUrl() {
+        return itemPicUrl;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public User getSeller() {
+        return seller;
+    }
+
+    public Bid[] getPlacedBids() {
+        return placedBids;
+    }
+
+    public Bid getHighestCurrentBid() {
+        return highestCurrentBid;
+    }
 }
